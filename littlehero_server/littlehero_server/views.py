fb7e6e1e63a4b1e8fdcb205ad71f4d0d48f7ec8c
jjiyeon83 <67498050+jjiyeon83@users.noreply.github.com>
2020-08-04 23:40:16 +0900

    Add files via upload

--- littlehero_server/littlehero_server/views.py
+++ littlehero_server/littlehero_server/views.py
@@ -1,4 +1,18 @@
-from django.shortcuts import render
+import django_filters
+from .filters import  SearchFilter, ViewFilter
+from rest_framework import filters
+from .models import Post
+from .serializers import SearchSerializer, ViewSerializer
+from rest_framework import generics
 
-def index(req):
-    return render(req, 'index.html')
+class PostView(generics.ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = ViewSerializer
+    filter_backends = [DjangoFilterBackend]
+    filter_class = ViewFilter
+
+class SearchView(generics.ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = SearchSerializer
+    filter_backends = [DjangoFilterBackend]
+    filter_class = SearchFilter
