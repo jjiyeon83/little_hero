e20a393bc1e8f5fdfd89d3b738aa202eef7576b5
jjiyeon83 <67498050+jjiyeon83@users.noreply.github.com>
2020-08-19 01:04:16 +0900

    Add files via upload

--- littlehero_server/announcement/views.py
+++ littlehero_server/announcement/views.py
@@ -37,7 +37,7 @@ class PostView(generics.ListAPIView) :
 
 
 # update likes
-def post_like(request):
+def post_like(request, pk):
     pk = request.POST.get('pk', None)
     post = get_object_or_404(Post, pk=pk)
     user = request.user
